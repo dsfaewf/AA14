88d42c5db1ce3329b6c75d10bbda0f90ed131239
dsfaewf <112606214+dsfaewf@users.noreply.github.com>
2022-10-27 17:01:45 +0900

    Add files via upload

--- aa14_rpt09/Node/cds_dht22/dbtest.js
+++ aa14_rpt09/Node/cds_dht22/dbtest.js
@@ -1,22 +1,22 @@
 // dbtest.js
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/", {
+mongoose.connect("mongodb://localhost/test", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 var SensorSchema = new mongoose.Schema({
-  data: ,
-  created: ,
+  data: String,
+  created: Date,
 });
 
 // data model
 var Sensor = mongoose.model("Sensor", SensorSchema);
 
-var sensor1 = new Sensor({ data: "124", created:  Date() });
+var sensor1 = new Sensor({ data: "124", created: new Date() });
 sensor1.save();
 
-var sensor2 = new Sensor({ data: "573", created:  Date() });
+var sensor2 = new Sensor({ data: "573", created: new Date() });
 sensor2.save();
 
 console.log("Sensor data were saved in MongoDB");
